7e9205ff8690a3eb414bb05376103be954e7cb68
GMoses <GMoses@DESKTOP-0J88KJ2>
2021-12-14 09:13:24 +0200

    Add project files.

---
+++ Tecky/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tecky.Data;
+
+[assembly: HostingStartup(typeof(Tecky.Areas.Identity.IdentityHostingStartup))]
+namespace Tecky.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
